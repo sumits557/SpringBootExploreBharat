d66852929e1f013aef9572eff3d835271e73b7c0
sumits557 <sumits557@gmail.com>
2020-05-13 19:28:54 -0400

    Initial commit for Spring boot application "Explore Bharat".

---
+++ src/main/java/com/example/eb/explorebharat/ExplorebharatApplication.java
@@ -0,0 +1,13 @@
+package com.example.eb.explorebharat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExplorebharatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExplorebharatApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/eb/explorebharat/ExplorebharatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.eb.explorebharat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExplorebharatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
